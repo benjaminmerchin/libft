8ff725841413da7d42886863ca6072018897ffcd
benjaminmerchin <benjaminmerchin@gmail.com>
2020-10-17 02:39:29 +0200

    WOTD

--- src/ft_putendl_fd.c
+++ src/ft_putendl_fd.c
@@ -2,22 +2,6 @@
 
 void ft_putendl_fd(char *s, int fd)
 {
-	int i;
-	int len;
-	unsigned char *str;
-
-	i = 0;
-	len = ft_strlen(s);
-	str = (unsigned char *)s;
-	if (!s)
-	{
-		write(fd, "\n", 1);
-		return ;
-	}
-	while (i < len)
-	{
-		write(fd, &str[i], 1);
-		i++;
-	}
+	ft_putstr_fd(s, fd);
 	write(fd, "\n", 1);
 }
