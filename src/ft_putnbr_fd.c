c727b3199c84e800e6bf2651f702d7ce497c8ce8
benjaminmerchin <benjaminmerchin@gmail.com>
2020-10-20 07:02:50 +0200

    = oublie

--- src/ft_putnbr_fd.c
+++ src/ft_putnbr_fd.c
@@ -10,7 +10,7 @@ void ft_putnbr_fd(int n, int fd)
 		ft_putchar_fd('-', fd);
 		nbr = -nbr;
 	}
-	if (nbr > 10)
+	if (nbr >= 10)
 	{
 		ft_putnbr_fd(nbr / 10, fd);
 		ft_putchar_fd(nbr % 10 + '0', fd);
